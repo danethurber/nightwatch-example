2271856a7ab458e8fbfc559e18d171e6943d2596
Dane Thurber <dane.thurber@gmail.com>
2015-12-11 11:00:06 -0700

    update to bdd style

--- ui-tests/specs/first.specs.js
+++ ui-tests/specs/first.specs.js
@@ -1,13 +1,13 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
-    browser
-      .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
-      .end()
+  'Demo test Google' : function(client) {
+    client.url('http://google.no').pause(1000)
+
+    client.expect.element('body').to.be.present.before(1000)
+    client.expect.element('#lst-ib').to.have.css('display')
+    client.expect.element('body').to.have.attribute('class').which.contains('vasq')
+    client.expect.element('#lst-ib').to.be.an('input')
+    client.expect.element('#lst-ib').to.be.visible
+
+    client.end()
   }
-};
+}
